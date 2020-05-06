483c58a80dcbdfbd553f56dbf3a23bb9e0e22283
imran82ali <code.imranali@gmail.com>
2020-05-06 08:40:29 +0500

    May 05 2020 Chapter 3 from Imran Ali

---
+++ may_05_2020_imran/literal/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let course = "PIAIC IOT"; // string literal , string slices
+    let quarter = 1;
+    println!("My course is {} and my quarter is : {}",course,quarter);
+    let course = course.len(); //method length //usize
+    println!("Length is : {}",course);
+}
---
+++ may_05_2020_imran/shadow/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let price1 = 300;  //implicit data type i32
+    // i32 bit = 4 byte
+    // 1 byte = 8 bit
+    println!("Today price is  {}",price1);
+    println!("addres of price1  {:p}",&price1);
+    let price1 = price1 + 100; 
+    println!("Today price is  {}",price1);
+    println!("addres of price1  {:p}",&price1);
+
+    let mut salary1:u16 = 10_000;  //10,000
+    println!("Salary 1 is  {}",salary1);
+    println!("address of Salary1  {:p}",&salary1);
+    salary1 = salary1+2000;
+    println!("Salary 1 is  {}",salary1);
+    println!("address of Salary1  {:p}",&salary1);
+    
+}
