5a1cb75ba2aaff9c39030400ce9c974c8fa0913e
AreebSiddiqui <areeb40@hotmail.com>
2020-05-31 14:42:19 +0500

    May-31-2020-MAreebSiddiqui

--- May-31-2020-MAreebSiddiqui/src/main.rs
+++ May-31-2020-MAreebSiddiqui/src/main.rs
@@ -1,13 +1,31 @@
 
-use std::io;
+// use std::io;
 
-//Referencing 
-fn main () {
-    let name = String::from("Areeb");
-    move_ownership(&name);
-    println!("{}",name);
+// //Referencing 
+// fn main () {
+//     let name = String::from("Areeb");
+//     move_ownership(&name);
+//     println!("{}",name);
+// }
+
+// fn move_ownership(data: &String) {
+//     println!("from the move ownership function: {}",data);
+// }
+
+//Illustrating Option Enum
+
+fn increment (num: Option<i32>) -> Option<i32> {
+    match num {
+        None => None,
+        Some(i) => Some(i+1)
+    }
 }
 
-fn move_ownership(data: &String) {
-    println!("from the move ownership function: {}",data);
-}
+fn main() {
+    let seven = Some(7);
+    println!("{:?}",seven);
+    let result = increment(seven);
+    println!("{:?}",result);
+    let nothing: Option<i32> = None;
+    println!("{:?}",nothing);
+}   
